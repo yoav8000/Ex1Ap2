6b064cf612a8f2448ad3bb5ad25d7fe54ab98499
yoav gomberg <yoav gomberg@DESKTOP-UQ08A7L>
2017-04-15 13:40:41 +0300

    made progression with state

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/Properties/CostComperator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib.Properties
+{
+    public class CostComperator<T>:IComparer<State<T>>
+    {
+        public static int  Compare(State<T> first, State<T> second)
+        {
+
+        } 
+    }
+}
---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/State.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib
+{
+    class State<T> : IComparable<T>
+    {
+        private double cost;
+        private State<T> cameFrom;
+        private T stateIdentifier;
+        public double Cost
+        {
+            get
+            {
+                return cost;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Value can not be negative");
+                }
+            }
+        }
+
+        public State<T> CameFrom
+        {
+            get
+            {
+                return cameFrom;
+            }
+            set
+            {
+                this.cameFrom = value;
+            }
+        }
+
+        public T StateIdentifier
+        {
+            get
+            {
+                return this.stateIdentifier;
+            }
+        }
+
+
+
+        public State(double cost, State<T> cameFrom)
+        {
+            this.cost = cost;
+            this.CameFrom = cameFrom;
+        }
+
+        public State()
+        {
+            this.cost = 0;
+            this.CameFrom = null;
+        }
+
+        public override int GetHashCode()
+        {
+            return String.Intern(this.stateIdentifier.ToString()).GetHashCode();
+        }
+
+
+        public bool Equals(State<T> other)
+        {
+            return this.stateIdentifier.Equals(other.StateIdentifier);
+           
+        }
+
+        public override bool Equals(object obj)////check if works fine.
+        {
+            if(obj.GetType() == typeof(State<T>))
+            {
+                return this.Equals(obj as State<T>);
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
